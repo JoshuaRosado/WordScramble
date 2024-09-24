d2c0d026e84d5e0e521b43b785631f00bc61e7fc
JoshuaRosado <joshua06rosado@gmail.com>
2024-09-24 06:37:54 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Joshua Rosado Olivencia on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
